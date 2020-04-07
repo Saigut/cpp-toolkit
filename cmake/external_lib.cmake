d5955b640a475a04b218d3a55528f3e63c3a25b2
Saigut <gosaigut@gmail.com>
2020-04-08 00:55:05 +0800

    Fix parallel argument of build

--- cmake/external_lib.cmake
+++ cmake/external_lib.cmake
@@ -4,6 +4,13 @@ include(ExternalProject)
 include(common_var)
 
 function(external_lib_setup)
+    # Parallel argument of build
+    if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.12")
+        set(PARALLEL_ARG "-j ${LCORES_PLUS1}")
+    elseif()
+        set(PARALLEL_ARG "")
+    endif()
+
     # json-c
     ExternalProject_Add(ep_json-c
         URL https://github.com/json-c/json-c/archive/master.zip
@@ -13,5 +20,5 @@ function(external_lib_setup)
             -DBUILD_SHARED_LIBS=OFF
             -DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_SOURCE_DIR}/external
             -DBUILD_TESTING=OFF
-        BUILD_COMMAND ${CMAKE_COMMAND} --build . -j ${LCORES_PLUS1})
+        BUILD_COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_ARG})
 endfunction()
