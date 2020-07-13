c0413c494a01728e67e2bb3267461aaccdc7f606
Saigut <gosaigut@gmail.com>
2020-07-13 22:09:01 +0800

    update for external lib

--- cmake/external_lib.cmake
+++ cmake/external_lib.cmake
@@ -3,7 +3,7 @@ include_guard()
 include(ExternalProject)
 include(common_var)
 
-function(external_lib_setup)
+function(external_lib_setup baseDir)
     # Parallel argument of build
     set(PARALLEL_ARG "")
     if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.12")
@@ -22,21 +22,21 @@ function(external_lib_setup)
 
     # json-c
     ExternalProject_Add(ep_json-c
-        URL https://github.com/json-c/json-c/archive/master.zip
-#        URL ${PROJECT_SOURCE_DIR}/package/json-c.zip
+#        URL https://github.com/json-c/json-c/archive/master.zip
+        URL ${baseDir}/package/json-c.zip
         DOWNLOAD_NAME json-c.zip
         CMAKE_ARGS
             -DBUILD_SHARED_LIBS=OFF
-            -DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_SOURCE_DIR}/external
+            -DCMAKE_INSTALL_PREFIX:PATH=${baseDir}/external
             -DBUILD_TESTING=OFF
-        BUILD_COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_ARG})
+        BUILD_COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_ARG} --target install)
 
     # boost_1_72_0
     ExternalProject_Add(ep_boost
 #        URL https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.zip
-        URL ${PROJECT_SOURCE_DIR}/package/boost_1_72_0.zip
+        URL ${baseDir}/package/boost_1_72_0.zip
         CONFIGURE_COMMAND ./bootstrap${BOOST_BS_SUFFIX}
         BUILD_IN_SOURCE true
         BUILD_COMMAND ""
-        INSTALL_COMMAND ./b2 --layout=system --prefix=${PROJECT_SOURCE_DIR}/external ${PARALLEL_ARG} address-model=64 architecture=x86 variant=release link=static install)
+        INSTALL_COMMAND ./b2 --layout=system --prefix=${baseDir}/external ${PARALLEL_ARG} address-model=64 architecture=x86 variant=release link=static install)
 endfunction()
