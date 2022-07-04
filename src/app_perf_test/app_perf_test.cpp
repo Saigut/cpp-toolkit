4bab91ac9801349a653235bc33d2bfe9f56ef217
Saigut <gosaigut@gmail.com>
2022-07-04 18:15:21 +0800

    add perf_test

---
+++ src/app_perf_test/app_perf_test.cpp
@@ -0,0 +1,1 @@
+#include "app_perf_test.h"
---
+++ src/app_perf_test/app_perf_test.h
@@ -0,0 +1,9 @@
+#ifndef CPP_TOOLKIT_APP_PERF_TEST_H
+#define CPP_TOOLKIT_APP_PERF_TEST_H
+
+// test performance of cpp
+// test lambda. create, assign, move, call
+// test share_ptr. create, assign, call
+// test boost::context::callcc. create, assign, return, resume
+
+#endif //CPP_TOOLKIT_APP_PERF_TEST_H
