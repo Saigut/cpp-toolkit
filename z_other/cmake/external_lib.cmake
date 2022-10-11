d22dacd1dcca907ea0b24faeb1a9f535f68bf695
Saigut <gosaigut@gmail.com>
2022-10-11 15:06:50 +0800

    disable some external libraries

--- z_other/cmake/external_lib.cmake
+++ z_other/cmake/external_lib.cmake
@@ -34,24 +34,24 @@ function(external_lib_setup baseDir)
     endif()
 
     # json-c
-    ExternalProject_Add(ep_json-c
-        URL ${V_JSON_C_URL}
-        DOWNLOAD_NAME json-c.zip
-        CMAKE_ARGS
-            -DBUILD_SHARED_LIBS=OFF
-            -DCMAKE_INSTALL_PREFIX:PATH=${baseDir}/external/${CMAKE_CXX_COMPILER_ID}
-            -DBUILD_TESTING=OFF
-            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        BUILD_COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_ARG})
+#    ExternalProject_Add(ep_json-c
+#        URL ${V_JSON_C_URL}
+#        DOWNLOAD_NAME json-c.zip
+#        CMAKE_ARGS
+#            -DBUILD_SHARED_LIBS=OFF
+#            -DCMAKE_INSTALL_PREFIX:PATH=${baseDir}/external/${CMAKE_CXX_COMPILER_ID}
+#            -DBUILD_TESTING=OFF
+#            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+#            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+#        BUILD_COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_ARG})
 
     # boost_1_79_0
-    ExternalProject_Add(ep_boost
-        URL ${V_BOOST_URL}
-        CONFIGURE_COMMAND ./bootstrap${BOOST_BS_SUFFIX}
-        BUILD_IN_SOURCE true
-        BUILD_COMMAND ""
-        INSTALL_COMMAND ./b2 --layout=system --prefix=${baseDir}/external/${CMAKE_CXX_COMPILER_ID} ${PARALLEL_ARG} address-model=64 architecture=x86 variant=release link=static install)
+#    ExternalProject_Add(ep_boost
+#        URL ${V_BOOST_URL}
+#        CONFIGURE_COMMAND ./bootstrap${BOOST_BS_SUFFIX}
+#        BUILD_IN_SOURCE true
+#        BUILD_COMMAND ""
+#        INSTALL_COMMAND ./b2 --layout=system --prefix=${baseDir}/external/${CMAKE_CXX_COMPILER_ID} ${PARALLEL_ARG} address-model=64 architecture=x86 variant=release link=static install)
 
     # grpc-1.34.0
     # set openssl path
@@ -61,16 +61,16 @@ function(external_lib_setup baseDir)
         set(V_grpc_cmake_ssl_param "-DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}")
     endif()
 
-    ExternalProject_Add(ep_grpc
-#        git clone -b v1.34.0 https://github.com/grpc/grpc
-        URL ${V_GRPC_URL}
-        DOWNLOAD_NAME grpc-1.34.0_with_deps.zip
-#        UPDATE_COMMAND git submodule update --init
-        CMAKE_ARGS
-            -DCMAKE_INSTALL_PREFIX:PATH=${baseDir}/external/${CMAKE_CXX_COMPILER_ID}
-            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-            -DgRPC_SSL_PROVIDER=package
-            ${V_grpc_cmake_ssl_param}
-        BUILD_COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_ARG})
+#    ExternalProject_Add(ep_grpc
+##        git clone -b v1.34.0 https://github.com/grpc/grpc
+#        URL ${V_GRPC_URL}
+#        DOWNLOAD_NAME grpc-1.34.0_with_deps.zip
+##        UPDATE_COMMAND git submodule update --init
+#        CMAKE_ARGS
+#            -DCMAKE_INSTALL_PREFIX:PATH=${baseDir}/external/${CMAKE_CXX_COMPILER_ID}
+#            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+#            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+#            -DgRPC_SSL_PROVIDER=package
+#            ${V_grpc_cmake_ssl_param}
+#        BUILD_COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_ARG})
 endfunction()
