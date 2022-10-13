872f0a3051564e8be54840c0a6628205508f655c
Saigut <gosaigut@gmail.com>
2022-10-13 18:12:10 +0800

    add app_prod_im

---
+++ include/app_prod_im/app_prod_im.h
@@ -0,0 +1,6 @@
+#ifndef CPP_TOOLKIT_APP_PROD_IM_H
+#define CPP_TOOLKIT_APP_PROD_IM_H
+
+int app_prod_im(int argc, char** argv);
+
+#endif //CPP_TOOLKIT_APP_PROD_IM_H
---
+++ src/app_prod_im/app_prod_im.cpp
@@ -0,0 +1,8 @@
+#include <app_prod_im/app_prod_im.h>
+
+
+
+int app_prod_im(int argc, char** argv)
+{
+    return -1;
+}
