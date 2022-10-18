6e0655984b2374b4231fa0ad7ce908f5f50c0319
Saigut <gosaigut@gmail.com>
2022-10-18 18:02:08 +0800

    update for app_prod_im

---
+++ src/app_prod_im/prod_im_client_contacts.cpp
@@ -0,0 +1,12 @@
+
+
+
+/*
+* 联系人管理
+  * 更新联系人列表
+    contact_update_list，参数：联系人列表 vector数组 引用
+  * 添加联系人
+    contact_add，参数：联系人ID 字符串；联系人备注 字符串；返回值：结果 int
+  * 移除联系人
+    contact_del，参数：联系人ID 字符串
+*/
---
+++ src/app_prod_im/prod_im_client_login_session.cpp
@@ -0,0 +1,12 @@
+
+
+
+/*
+* 登录会话
+  * 开启会话
+    login_session_open，参数：IO连接 socket；返回值：结果 int
+  * 关闭会话
+    login_session_close
+  * 获取IO连接
+    login_session_get_io_port
+*/
