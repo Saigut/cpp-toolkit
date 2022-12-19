f82885e79163ab8819f49c85cf0861efe760f6ad
Saigut <gosaigut@gmail.com>
2022-12-20 00:56:41 +0800

    mod_np_queue: update

--- include/mod_np_queue/mod_np_queue.hpp
+++ include/mod_np_queue/mod_np_queue.hpp
@@ -6,18 +6,20 @@
 
 using np_queue_aq_t = atomic_queue::AtomicQueue2<void*, 65535>;
 
-// Notify to polling queue
+// Notify to polling queue。多写单读
 class np_queue_t {
 public:
     np_queue_t(std::function<void()>&& notify_handler,
-               std::function<void(void*)> dequeue_handler)
+               std::function<bool(void*)>&& dequeue_handler,
+               std::function<bool()>&& waiting_handler)
                : m_notify_handler(notify_handler),
-                 m_dequeue_handler(dequeue_handler)
-               {}
+                 m_dequeue_handler(dequeue_handler),
+                 m_waiting_handler(waiting_handler) {}
     int enqueue(void* p);
-    int dequeue();
-    int dequeue_wait();
-    int dequeue_co();
+
+    // ret: true, 正常退出; false, 提前退出
+    bool do_dequeue();
+    int do_dequeue_wait();
 
 private:
     np_queue_aq_t m_queue;
@@ -26,7 +28,9 @@ private:
     std::atomic<bool> m_notified = false;
 
     std::function<void()> m_notify_handler;
-    std::function<void(void*)> m_dequeue_handler;
+    std::function<bool(void*)> m_dequeue_handler;
+    // ret: true, 继续 dequeue; false, 不 dequeue 了
+    std::function<bool()> m_waiting_handler;
 };
 
 
--- src/mod_np_queue/mod_np_queue.cpp
+++ src/mod_np_queue/mod_np_queue.cpp
@@ -8,11 +8,14 @@ int np_queue_t::enqueue(void* p)
         return -1;
     }
     // 2. 检查模式。notify 模式则通知。
+    /// TODO: 多线程支持
     if (m_is_notify_mode) {
         if (!m_notified) {
             // notify
             if (m_notify_handler) {
                 m_notify_handler();
+            } else {
+                return -1;
             }
             m_notified = true;
         }
@@ -20,7 +23,7 @@ int np_queue_t::enqueue(void* p)
     return 0;
 }
 
-int np_queue_t::dequeue()
+bool np_queue_t::do_dequeue()
 {
     void* p;
     // 1. 切换为 poll 模式
@@ -29,7 +32,9 @@ int np_queue_t::dequeue()
     while (m_queue.try_pop(p)) {
         // call handler
         if (m_dequeue_handler) {
-            m_dequeue_handler(p);
+            if (!m_dequeue_handler(p)) {
+                return false;
+            }
         }
     }
     // 3. 切换为 notify 模式
@@ -37,26 +42,26 @@ int np_queue_t::dequeue()
     while (!m_notified && m_queue.try_pop(p)) {
         // call handler
         if (m_dequeue_handler) {
-            m_dequeue_handler(p);
+            if (!m_dequeue_handler(p)) {
+                return false;
+            }
         }
     }
-    return 0;
+    return true;
 }
 
-int np_queue_t::dequeue_wait()
+int np_queue_t::do_dequeue_wait()
 {
-    while (true) {
-        dequeue();
+    if (!m_waiting_handler) {
+        return -1;
+    }
+
+    do {
+        if (!do_dequeue()) {
+            return 0;
+        }
         // 等待 condition 信号
-    }
-    return 0;
-}
+    } while (m_waiting_handler());
 
-int np_queue_t::dequeue_co()
-{
-    while (true) {
-        dequeue();
-        // 协程等待信号
-    }
     return 0;
 }
